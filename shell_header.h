3e476ba1d06a171b95c8574961f05c07f16362db
SN Bukhali <sphaluk56@gmail.com>
2023-12-13 15:39:17 +0200

    Wrap all header files into 1 header file for ease of management

---
+++ shell_header.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024 /* Maximum size of the input buffer */
+
+#include "shell.h"
+#include "executor.h"
+
+#endif
